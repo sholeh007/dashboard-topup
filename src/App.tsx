71e921a19749cf2d11dc359146dfb36074de8dfd
sholeh007 <sholeh09@outlook.com>
2021-12-09 12:29:02 +0900

    fix layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Footer from "./components/organisms/footer";
 import MainContent from "./components/organisms/main-content";
 import Navbar from "./components/organisms/navbar";
 import Sidebar from "./components/organisms/sidebar";
+import styles from "./app.module.css";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Navbar />
       <div id="layoutSidenav">
         <Sidebar />
-        <div id="layoutSidenav_content">
+        <div id="layoutSidenav_content" className={styles.content}>
           <MainContent />
           <Footer />
         </div>

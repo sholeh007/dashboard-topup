efefb5da5cf260c7bd8fa2424279143766f656a8
sholeh007 <sholeh09@outlook.com>
2021-12-14 10:17:04 +0900

    refactoring sidebar

---
+++ src/components/organisms/sidebar/list-menu.ts
@@ -0,0 +1,30 @@
+export const Master = [
+  {
+    title: "category",
+    link: "category",
+    icon: "fa-clipboard-list",
+  },
+  {
+    title: "nominal",
+    link: "nominal",
+    icon: "fa-tasks",
+  },
+  {
+    title: "voucher",
+    link: "voucher",
+    icon: "fa-tags",
+  },
+];
+
+export const Pembanyaran = [
+  {
+    title: "bank",
+    link: "bank",
+    icon: "fa-university",
+  },
+  {
+    title: "metode pembanyaran",
+    link: "pembanyaran",
+    icon: "fa-credit-card",
+  },
+];

9a6320ec04351c1503b7755952b209d10ac539da
sholeh007 <sholeh09@outlook.com>
2021-12-23 12:15:02 +0900

    refactor api singleCategory

--- src/api/category.ts
+++ src/api/category.ts
@@ -20,8 +20,8 @@ export async function delCategory(id: string) {
   return json;
 }
 
-export async function singleCategory(id: string | undefined) {
-  const json = await ky.get(`${config.api}/category?id=${id}`).json();
+export async function singleCategory(id: string) {
+  const json = await ky.get(`${config.api}/category/${id}`).json();
   return json;
 }
 
